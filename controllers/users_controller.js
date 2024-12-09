cf97e31fb06565008d5def8365e9f567a08d7f81
S-Khan786 <osahilkhan654@gmail.com>
2024-12-09 09:50:11 +0530

    Rendering pages for signin and signup

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,17 @@
 module.exports.profile = function(req, res) {
     res.end('<h1>User Profile<\h1>')
 }
+
+// render the sign up page
+module.exports.signUp = function(req, res) {
+    res.render('user_sign_up', {
+        title: 'todoList | Sign Up'
+    })
+}
+
+// render the sign in page
+module.exports.signIn = function(req, res) {
+    res.render('user_sign_in', {
+        title: 'todoList | Sign In'
+    })
+}
